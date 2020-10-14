130ef58bdc267e9ae681f3be1eaf0899c6604c7f
Jake Price <jp@jakelprice.com>
2020-10-14 09:36:35 +0100

    Integrate Tests (#1)
    * Inital
    
    * Scaffolding for testing

---
+++ tests/Unit/PageviewTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Unit;
+
+use Jakeydevs\Analytics\Tests\TestCase;
+
+class PageviewTest extends TestCase
+{
+    /**
+     * Can we store a pageview from a request
+     *
+     * @test
+     * @group analaytics
+     */
+    public function CanStorePageview()
+    {
+        $this->assertTrue(true);
+    }
+}
