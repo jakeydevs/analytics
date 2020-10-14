c73d8a4971e608cf12c0d72d227df9f825e3da8c
jakey <jp@jakelprice.com>
2020-10-14 09:35:48 +0100

    Scaffolding for testing

---
+++ tests/TestCase.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Jakeydevs\Analytics\Tests;
+
+use Jakeydevs\Analytics\AnalyticServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            AnalyticServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+}
